31cd2202af77bcb10f86051cfa7b1951e6315d99
Charles Morman <github@cmorman.com>
2024-12-20 19:39:34 -0700

    feat: update style with tailwind

--- app/app.py
+++ app/app.py
@@ -33,9 +33,9 @@ def home():
 
     if app.config.get("TESTING"):
         return jsonify(weather=weather, error=error)
-    
+
     return render_template("home.html", weather=weather, error=error)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
